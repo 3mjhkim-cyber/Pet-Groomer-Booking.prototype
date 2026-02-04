76aa0240343e69c56a9bd743aa7119eb0bd8b6fe
김제훈 <3mjhkim@gmail.com>
2026-02-04 17:16:50 +0900

    로컬 개발 환경 및 서버 설정 수정

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv";
+dotenv.config;
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
@@ -94,7 +96,7 @@ app.use((req, res, next) => {
   httpServer.listen(
     {
       port,
-      host: "0.0.0.0",
+      host: "127.0.0.1",
       reusePort: true,
     },
     () => {
